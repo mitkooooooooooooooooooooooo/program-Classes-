ee28076574b9ad7f3b39c7500d5aa8efc198770f
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-03 12:04:57 +0300

    task num 3

---
+++ exact sum of real num/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exact_sum_of_real_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ exact_sum_of_real_num/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exact_sum_of_real_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < input; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
