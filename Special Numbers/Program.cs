e4dbb266761673c950ba840a78958a7193ee6ce7
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-03 13:16:40 +0300

    special nums

---
+++ Special Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1 ; i < input; i++)
+            {
+                int num = i;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+                sum = 0;
+            }
+        }
+    }
+}
