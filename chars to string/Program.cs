d732513628bee33013cdae6e69fe63176879d4ae
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-03 13:25:40 +0300

    char to string

---
+++ chars to string/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace chars_to_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+
+            for(int i = 0; i < 3; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                output += input;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
