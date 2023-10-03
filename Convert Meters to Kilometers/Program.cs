b4a49d03e083aebcbb2c9c56a5fcbde90adcc316
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-03 11:42:36 +0300

    added task 1

---
+++ Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal output = input / 1000;
+            Console.WriteLine($"{output:F2}");
+        }
+    }
+}
