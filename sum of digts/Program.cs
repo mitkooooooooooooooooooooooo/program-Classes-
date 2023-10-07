ebe4d24a0d8a0cc006b13b6c1c17d3eadab112cd
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-07 15:56:56 +0300

    sum of digts

---
+++ sum of digts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace sum_of_digts
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+
+}
