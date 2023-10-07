9db9778a6610a7646c3b08c7a9ee435c5414b208
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-07 16:08:41 +0300

    elevator

---
+++ 03. Elevator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Elevator
+{
+
+  class Program
+  {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            int coursesNeeded = (int)Math.Ceiling((double)n / p);
+
+            Console.WriteLine(coursesNeeded);
+        
+        
+        
+        
+        
+        
+        
+        }
+  }
+   
+}
