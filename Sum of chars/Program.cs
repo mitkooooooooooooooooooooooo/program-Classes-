ac0bd60cf0f6f914a88789204199aa46eb141bea
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-07 16:19:24 +0300

    sum of chars

---
+++ Sum of chars/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sum_of_chars
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                totalSum += (int)character;
+            }
+
+            Console.WriteLine("The sum equals: " + totalSum);
+        }
+    }
+
+}
