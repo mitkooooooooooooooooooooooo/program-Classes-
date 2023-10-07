79258dc4c87ec3c9444b351d591761cc72c4a2fa
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-07 15:27:17 +0300

    integer operations

---
+++ integer operations/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace integer_operations
+{
+
+    class Program
+    {
+        static void Main()
+        {
+           
+           
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+             
+            
+            int num4 = int.Parse(Console.ReadLine()); 
+
+            
+            int sum = num1 + num2;
+            int divisionResult = sum / num3;
+           
+            int finalResult = divisionResult * num4;
+
+            
+            Console.WriteLine("Result: " + finalResult);
+
+        }
+    }
+
+}
