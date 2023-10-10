cd24982342ab5b88a574354c1e657b4445821633
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-10 12:26:38 +0300

    zig zag

---
+++ Zig zag rAYS/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Zig_zag_rAYS
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+           
+            for (int i = 0; i < n; i++)
+            {
+               
+                string[] input = Console.ReadLine().Split();
+                int firstNumber = int.Parse(input[0]);
+                int secondNumber = int.Parse(input[1]);
+
+                
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = firstNumber;
+                    secondArr[i] = secondNumber;
+                }
+                else
+                {
+                    firstArr[i] = secondNumber;
+                    secondArr[i] = firstNumber;
+                }
+            }
+
+         
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+        }
+    }
+
+}
