abc2913aea2260f6e4a6966b3e1636e309a302cd
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-10-24 13:20:53 +0300

    hangmun

---
+++ 3. Nothing in common/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _3._Nothing_in_common
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    int num1 = int.Parse(input[i]);
+                    int num2 = int.Parse(input[j]);
+                    int sum = num1 + num2;
+
+                    if (sum == targetSum)
+                    {
+                        Console.WriteLine($"{num1} {num2}");
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Hangman 11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hangman_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
